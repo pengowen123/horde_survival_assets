ae1dc6df035ef4fc82c1cbf2c322a8060901cac1
Owen Sanchez <pengowen816@gmail.com>
2017-01-01 14:53:02 -0700

    Convert files to unix format

--- shaders/150/f_3d.glsl
+++ shaders/150/f_3d.glsl
@@ -6,6 +6,6 @@ in vec2 v_TexCoord;
 
 void main() {
     vec4 tex = texture(t_Color, v_TexCoord);
-    float blend = dot(v_TexCoord-vec2(0.5,0.5), v_TexCoord-vec2(0.5,0.5));
-    gl_FragColor = mix(tex, vec4(0.0,0.0,0.0,0.0), blend*1.0);
+    float blend = dot(v_TexCoord - vec2(0.5, 0.5), v_TexCoord - vec2(0.5, 0.5));
+    gl_FragColor = mix(tex, vec4(0.0, 0.0, 0.0, 0.0), blend * 1.0);
 }
